233edb486ea1760ea04efbf5db58f57a38fcda26
jiwonz <jiwonz0113@gmail.com>
2023-04-17 14:47:19 +0000

    base

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { IntentsBitField, Collection } from "discord.js"
 import { BotClient } from "./classes/BotClient"
 import * as fs from "fs"
 
-//test
-
 const config:{
     TOKEN:string,
     CLIENT_ID:string
