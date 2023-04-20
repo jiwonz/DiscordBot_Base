145dcc1163daa28ea59925d222655ac5aed004bf
jiwonz <jiwonz0113@gmail.com>
2023-04-20 15:03:38 +0000

    interaction update

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -11,7 +11,7 @@ export default {
             if (client.isDeveloper(interaction.member.user.id)) {
                 await command.execute(client, interaction)
             } else {
-                await interaction.reply({ content: "봇이 개발 모드에 있습니다. 봇 개발자만 사용할 수 있습니다.", ephemeral: true })
+                await interaction.reply({ content: "현재 봇이 개발 모드에 있습니다. 봇 개발자만 이용할 수 있습니다.", ephemeral: true })
             }
         } catch (error) {
             console.log(error)
