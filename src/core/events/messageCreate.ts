4cf300c18d10f15133b1db785368e81317abc206
jiwonz <jiwonz0113@gmail.com>
2023-06-05 17:10:24 +0000

    update message create

--- src/core/events/messageCreate.ts
+++ src/core/events/messageCreate.ts
@@ -4,6 +4,7 @@ import { config } from "../../index"
 
 export default {
     async execute(client:BotClient,message:Message) {
+        if (!client.isDeveloper(message.author.id)) return
         const args = message.content.split(" ")
 
         if (args[0][0] === config.DEV_COMMAND_PREFIX) {
@@ -12,7 +13,7 @@ export default {
 
             const cmd = client.developerCommands[args[0]]
             if (cmd != null) {
-                cmd.execute(client,message)
+                cmd.execute(client,args,message)
             }
         }
     },
