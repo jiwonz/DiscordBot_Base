693e097dc69b8d7e55951a78e67b7a373aceb5b2
jiwonz <jiwonz0113@gmail.com>
2023-06-05 14:09:23 +0000

    added message create

---
+++ src/core/events/messageCreate.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import { BotClient } from "../classes/BotClient"
+import { config } from "../../index"
+
+export default {
+    async execute(client:BotClient,message:Message) {
+        const args = message.content.split(" ")
+
+        if (args[0][0] === config.DEV_COMMAND_PREFIX) {
+
+            console.log(JSON.stringify(client.developerCommands))
+
+            const cmd = client.developerCommands[args[0]]
+            if (cmd != null) {
+                cmd.execute(client,message)
+            }
+        }
+    },
+};
