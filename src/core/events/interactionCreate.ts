eee5dbe93b8f9b2014b749e4d063e22682224e9e
jiwonz <jiwonz0113@gmail.com>
2023-06-05 17:26:59 +0000

    fix error

--- src/core/events/interactionCreate.ts
+++ src/core/events/interactionCreate.ts
@@ -1,13 +1,13 @@
-import { Interaction } from "discord.js"
+import { Interaction, InteractionType } from "discord.js"
 import { BotClient } from "../classes/BotClient"
 import { config } from "../../index"
 
 export default {
     async execute(interaction:Interaction, client:BotClient) {
-        if (!interaction.isCommand()) return
+        if (!interaction.interaction.type === InteractionType.ApplicationCommand) return
         const command = client.commands.get(interaction.commandName)
         if (!command) return
-        
+
         try{
             if (config.STATUS.InDev && !client.isDeveloper(interaction.member.user.id)) {
                 await interaction.reply({ content: config.IN_DEV_MESSAGE, ephemeral: true })
@@ -17,9 +17,9 @@ export default {
         } catch (error) {
             console.log(error)
             await interaction.reply({
-                content: "There was an error while executing this command!", 
+                content: "There was an error while executing this command!",
                 ephemeral: true
             })
-        } 
+        }
     },
 }
