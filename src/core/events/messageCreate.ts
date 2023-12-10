335f75282bfc3d9c00d36fc4d832ee4cb53ff0de
jiwonz <jiwonz0113@gmail.com>
2023-12-10 15:57:19 +0000

    executeRaw(client,message) added

--- src/core/events/messageCreate.ts
+++ src/core/events/messageCreate.ts
@@ -3,12 +3,11 @@ import { BotClient } from "../classes/BotClient"
 import { config } from "../../index"
 
 export default {
-    async execute(client:BotClient,message:Message) {
+	async execute(client:BotClient,message:Message) {
 		if (message.author.id === client.user.id) return
 
-		const firstSplit = message.content.split(' ');
-		const args = firstSplit.map(item => item.split('\n')).flat();
-		console.log("args debug log:",args)
+		const args = message.content.split(" ");
+
 		if (args[0][0] === config.COMMAND_PREFIX) {
 
 			console.log("prefix commands debug log:",JSON.stringify(client.prefixCommands))
@@ -16,23 +15,29 @@ export default {
 			const cmd = client.prefixCommands[`${args[0]}`]
 			if (cmd != null) {
 				args.splice(0,1)
-				cmd.execute(client,args,message)
+				if (cmd.execute != null)
+					cmd.execute(client,args,message)
+				if (cmd.executeRaw != null)
+					cmd.executeRaw(client,message)
 				return
 			}
 		}
 
-        if (!client.isDeveloper(message.author.id)) return
-        if (!(message.channel instanceof DMChannel)) return
+		if (!client.isDeveloper(message.author.id)) return
+		if (!(message.channel instanceof DMChannel)) return
 
-        if (args[0][0] === config.DEV_COMMAND_PREFIX) {
+		if (args[0][0] === config.DEV_COMMAND_PREFIX) {
 
-            console.log("dev commands debug log:",JSON.stringify(client.developerCommands))
+			console.log("dev commands debug log:",JSON.stringify(client.developerCommands))
 
-            const cmd = client.developerCommands[`${args[0]} ${args[1]}`]
-            if (cmd != null) {
-                args.splice(0,1)
-                cmd.execute(client,args,message)
-            }
-        }
-    },
+			const cmd = client.developerCommands[`${args[0]} ${args[1]}`]
+			if (cmd != null) {
+				args.splice(0,1)
+				if (cmd.execute != null)
+					cmd.execute(client,args,message)
+				if (cmd.executeRaw != null)
+					cmd.executeRaw(client,message)
+			}
+		}
+	},
 };
